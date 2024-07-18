6a8828c6fa8a98d94f001f46074474f8c4875cf5
Osiomhele Ogwanighie <osiomhele.ogwanighie@ihstowers.com>
2024-07-18 12:26:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const inputBox = document.getElementById("input-box");
+const list = document.getElementById("List");
+var btn = document.getElementById("btn")
+btn.addEventListener("click", click)
+
+function click(){
+    if(inputBox.value === ""){
+    alert("You must write something!");
+    }
+    else{
+    let li = document.createElement("li");
+    li.innerText = inputBox.value;
+    list.appendChild(li);
+    let span = document.createElement("span")
+    span.innerHTML = "\u00d7"
+    li.appendChild(span)
+    }
+    inputBox.value = ""
+    saveData()
+}
+
+list.addEventListener("click", function(e){
+    if (e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+        saveData()
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove()
+        saveData()
+    }
+},false)
+
+function saveData(){
+    localStorage.setItem("data", list.innerHTML)
+}
+
+function showData(){
+    list.innerHTML = localStorage.getItem("data")
+}
+
+showData()
